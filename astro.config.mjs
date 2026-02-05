182e56582d8c9b0e8cc60d1019de27ccafc1e675
ito-ux <ito@drrobo.ai>
2026-02-05 19:32:19 +0900

    no message

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  output: "static",         // ← これにする（または行ごと削除でもOK）
+  output: 'static',
   site: "https://help.drrobo.ai/",
-  adapter: cloudflare(),
   integrations: [tailwind()],
 });
